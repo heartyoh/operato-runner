aa0efec0bb3b2514feb66172384bffeb4c846ab4
Hearty, Oh <heartyoh@hatiolab.com>
2025-07-15 04:43:29 +0900

    fix: 1.1.13

--- alembic/versions/d75cf45ba241_add_is_active_column_to_user_table.py
+++ alembic/versions/d75cf45ba241_add_is_active_column_to_user_table.py
@@ -19,8 +19,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column('user', sa.Column('is_active', sa.Boolean(), nullable=False, server_default=sa.true()))
+    op.add_column('users', sa.Column('is_active', sa.Boolean(), nullable=False, server_default=sa.true()))
 
 
 def downgrade() -> None:
-    op.drop_column('user', 'is_active')
+    op.drop_column('users', 'is_active')
